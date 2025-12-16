c191b909384c2031455d5ee9d9ad4e97ce31389d
Gaganpreet <gaganpreet.singh@icodelabs.co>
2025-12-16 14:14:58 +0530

    refactor(navigation): Comment out unused animation and presentation options in navigationConfig
    - Disable animation and presentation options in navigationConfig for clarity and to prevent confusion.
    - Maintain existing comments for potential future use or reference.

--- src/navigators/navigationConfig.ts
+++ src/navigators/navigationConfig.ts
@@ -5,8 +5,8 @@ import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 const navigationConfig: NativeStackNavigationOptions = {
   headerShown: false,
   gestureEnabled: true,
-  animation: 'fade', // 'default' | 'fade' | 'slide_from_right' | 'slide_from_left' | 'slide_from_bottom' | 'none'
-  presentation: 'card', // 'card' | 'modal' | 'transparentModal' | 'containedModal' | 'containedTransparentModal' | 'fullScreenModal' | 'formSheet'
+  // animation: 'fade', // 'default' | 'fade' | 'slide_from_right' | 'slide_from_left' | 'slide_from_bottom' | 'none'
+  // presentation: 'card', // 'card' | 'modal' | 'transparentModal' | 'containedModal' | 'containedTransparentModal' | 'fullScreenModal' | 'formSheet'
   // statusBarAnimation: 'slide',
   // Note: Native Stack doesn't support custom interpolators like Stack Navigator
   // It uses native transitions which are more performant
